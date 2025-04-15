4fa42a9f5767977b308658ed3deb09f59d462cbf
Camellia.G <edeward@gmail.com>
2025-04-15 13:42:01 +0900

    Update emoov_app_assets.dart

--- lib/emoov_app_assets.dart
+++ lib/emoov_app_assets.dart
@@ -2,6 +2,7 @@ library emoov_app_assets;
 
 export 'src/pages/app_info_page.dart';
 
+export 'src/data_models/app_user.dart';
 export 'src/data_models/receivable_payment_record.dart';
 export 'src/data_models/charging_record.dart';
 export 'src/data_models/charger.dart';
