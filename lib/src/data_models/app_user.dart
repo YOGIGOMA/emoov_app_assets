6815c6ede5034308bbfa461178d6db5087feb42c
Camellia.G <edeward@gmail.com>
2025-04-15 13:41:03 +0900

    Create app_user.dart

---
+++ lib/src/data_models/app_user.dart
@@ -0,0 +1,37 @@
+
+/// @{category DataModel}
+/// 사용자인증정보 데이터모델
+class AppUser
+{
+  String username = '';
+  String signInId = '';
+  String token = '';
+  String sysId = '';
+  String refreshToken = '';
+  String nickName = '';
+  String expiredDate = '';
+
+  AppUser({
+    required this.username,
+    required this.token,
+    required this.signInId,
+    required this.sysId,
+    required this.refreshToken,
+    required this.nickName,
+    required this.expiredDate
+  });
+
+  factory AppUser.fromJson(Map<String, dynamic> jsonData)
+  {
+    return AppUser(
+        sysId: jsonData['UserSysId'],
+        signInId: jsonData['SignInId'],
+        username: jsonData['UserName'],
+        nickName: jsonData['NickName'],
+        token: jsonData['AccessToken'],
+        refreshToken: jsonData['RefreshToken'],
+        expiredDate: jsonData['ExpiredDate']
+    );
+  }
+
+}
