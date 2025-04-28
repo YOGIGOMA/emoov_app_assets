238f2575fd537bf02b555f708795f672a16d817a
Camellia.G <edeward@gmail.com>
2025-04-28 18:12:06 +0900

    Create identity_verification_info.dart

---
+++ lib/src/data_models/identity_verification_info.dart
@@ -0,0 +1,13 @@
+
+/// @{category DataModel}
+/// 본인인증 정보
+class IdentityVerificationInfo
+{
+  String realName = '';
+  String phoneNumber = '';
+
+  IdentityVerificationInfo({
+    required this.realName,
+    required this.phoneNumber
+  });
+}
