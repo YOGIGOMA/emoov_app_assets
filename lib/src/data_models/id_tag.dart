0d877d56f35c8fd60b6229912f244811d634f079
Camellia.G <edeward@gmail.com>
2025-06-15 17:29:44 +0900

    Update id_tag.dart

--- lib/src/data_models/id_tag.dart
+++ lib/src/data_models/id_tag.dart
@@ -8,6 +8,8 @@ class IdTag {
   bool isBlocked = false;
   bool isDeleted = false;
   String regDate = '';
+  bool isMobileCardTagId = false;
+  String issueMethod = '';
 
   IdTag({
     required this.tagSysId,
@@ -17,6 +19,8 @@ class IdTag {
     required this.isBlocked,
     required this.isDeleted,
     required this.regDate,
+    required this.isMobileCardTagId,
+    required this.issueMethod,
   });
 
   factory IdTag.fromJson(Map<String, dynamic> jsonData) {
@@ -31,6 +35,25 @@ class IdTag {
       isBlocked: jsonData['isBlocked'],
       isDeleted: jsonData['isDeleted'],
       regDate: splits[0].trim(),
+      isMobileCardTagId: false,
+      issueMethod: '',
+    );
+  }
+
+  factory IdTag.fromVersion2Json(Map<String, dynamic> jsonData) {
+
+    final splits = (jsonData['createdDate'] as String).split('T');
+
+    return IdTag(
+      tagSysId: jsonData['tagId'],
+      idTag: jsonData['idTag'],
+      tagType: jsonData['tagType'],
+      isUsed: jsonData['used'],
+      isBlocked: jsonData['isBlocked'],
+      isDeleted: jsonData['isDeleted'],
+      regDate: splits[0].trim(),
+      isMobileCardTagId: jsonData['isOurIssued'],
+      issueMethod: jsonData['issueMethod'],
     );
   }
 }
