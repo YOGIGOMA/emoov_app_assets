190a1062dd889a15bd3e850fe99a0dfa5429fcdc
Camellia.G <edeward@gmail.com>
2025-12-20 01:35:44 +0900

    Update connection_error.dart

--- lib/src/widgets/connection_error.dart
+++ lib/src/widgets/connection_error.dart
@@ -9,25 +9,50 @@ class ConnectionError extends StatelessWidget
   @override
   Widget build(BuildContext context)
   {
-    return const Center(
+    return Container(
+        width: double.infinity,
+        margin: EdgeInsets.zero,
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        decoration: const BoxDecoration(
+          color: Colors.transparent,
+        ),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Icon(Icons.warning,
-              color: Colors.deepOrange,
-              weight: 40,
-              size: 30,
-            ),
-            const SizedBox(height: 10,),
-            Text('서비스 연결중 오류가 발생 했습니다.',
-              style: TextStyle(
-                fontWeight: FontWeight.w500,
-                fontSize: 15,
-                color: Color(0xff3D3D3D),
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.warning_amber_rounded,
+                size: 50.sp,
+                color: Colors.redAccent,
               ),
-            ),
-          ],
+              const SizedBox(height: 10,),
+              Text('죄송합니다.',
+                  style: TextStyle(
+                    fontSize: 23.sp,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.black,
+                  )
+              ),
+              const SizedBox(height: 10,),
+              Text('서버와 일시적으로\n접속 할 수 없습니다.',
+                style: TextStyle(
+                  fontSize: 23.sp,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.black,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 30,),
+              Text('동일한 문제가 발생하는 경우는\n'
+                  '앱을 완전히 종료후 다시 시작해 보시기 바랍니다.',
+                style: TextStyle(
+                  fontSize: 15.sp,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.black,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ]
         )
     );
   }
